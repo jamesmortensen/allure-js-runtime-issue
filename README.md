1fb1dba3bfcf7a037700dc9403fb38ea28737495
James Mortensen <jmort253@gmail.com>
2022-10-28 13:22:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+This repo reporoduces an issue in allure-mocha. 
+
+Install deps:
+```
+npm install
+```
+
+Run tests:
+
+```
+npm test
+```
+
+Observe that there is an exception because `allure` is undefined.
+
+
+
---
+++ test/example.spec.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+
+// const MochaAllureReporter = require('allure-mocha');
+// console.log(MochaAllureReporter);
+
+
+// //const allurejs = require('allure-js-commons');
+// const allureRuntime = require('allure-mocha/runtime');
+// console(Mocha.Runner);
+// const allureReporter = new MochaAllureReporter(allureRuntime);
+// const MochaAllure = require('allure-mocha/dist/MochaAllure');
+// console.log(allurejs);
+
+const allure = require("allure-mocha/runtime");
+
+describe('Test', () => {
+
+    it('should pass', () => {
+        //const allure = new MochaAllure(allureReporter, allureRuntime);
+        console.log(allure);
+        allure.epic('something happening');
+        //allure.story('tell me a story');
+        allure.description('Hacking successful!');
+        console.log(allure);
+    });
+
+
+    it('should fail', () => { 
+        expect(0).to.equal(1);
+    })
+
+});
